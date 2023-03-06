7912d8458cd0d8ce894d6e5b79e50faf3dcc03cf
Djzet <silaevegor2003@mail.ru>
2023-03-06 14:37:45 +0700

    practical_work_4

--- js/main.js
+++ js/main.js
@@ -5,7 +5,17 @@ let app = new Vue({
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100,
-        onSale:true
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
